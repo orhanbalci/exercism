39ad84861728f3263745d4dd942859d15f80b781
orhanbalci@gmail.com <orhanbalci@gmail.com>
2019-07-25 19:46:21 +0300

    rust bob solution updated

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,20 +1,23 @@
 pub fn reply(message: &str) -> &str {
-    match message {
-        _x if message.trim().len() == 0 => "Fine. Be that way!",
-        _x if is_question(message) && is_yelling(message) => "Calm down, I know what I'm doing!",
-        _x if is_question(message) => "Sure.",
-        _x if is_yelling(message) => "Whoa, chill out!",
+    let message = message.trim();
+    let (question, yelling) = (is_question(message), is_yelling(message));
+
+    if message.is_empty() {
+        return "Fine. Be that way!";
+    }
+    match (question, yelling) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, _) => "Sure.",
+        (_, true) => "Whoa, chill out!",
         _ => "Whatever.",
     }
 }
 
 pub fn is_question(s: &str) -> bool {
-    s.trim().chars().last().unwrap() == '?'
+    s.ends_with("?")
 }
 
 pub fn is_yelling(s: &str) -> bool {
-    s.trim()
-        .chars()
-        .all(|c| c.is_uppercase() || !c.is_alphabetic())
+    s.chars().all(|c| c.is_uppercase() || !c.is_alphabetic())
         && s.chars().any(|c| c.is_alphabetic() && c.is_uppercase())
 }
