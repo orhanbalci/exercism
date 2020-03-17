2a3f941e303e22bc9f9b425feb9c197edc5ebabc
orhanbalci@gmail.com <orhanbalci@gmail.com>
2020-03-17 23:37:43 +0300

    nth prime rust solution updates

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -3,11 +3,16 @@ use std::vec::Vec;
 
 pub fn nth(n: u32) -> u32 {
     let mut primes = Vec::new();
-    for i in 2..u32::max_value() {
+    for i in 2.. {
         if primes.len() == usize::try_from(n).unwrap() + 1 {
             return *primes.last().unwrap();
         }
-        if primes.iter().find(|&&x| i % x == 0) == None || i == 2 {
+        if primes
+            .iter()
+            .filter(|&&s| s <= (i as f32).sqrt().ceil() as u32)
+            .all(|&x| i % x != 0)
+            || i == 2
+        {
             primes.push(i);
         }
     }
