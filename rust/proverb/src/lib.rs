453494128b5a2302302af4e7e56f80ec362518a7
orhanbalci@gmail.com <orhanbalci@gmail.com>
2020-03-30 21:02:52 +0300

    proverb rust solution second iteration

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.len() == 0 {
+    if list.is_empty() {
         return "".to_string();
     }
 
-    let windows = list.windows(2);
     let mut resultv = Vec::new();
 
-    for window in windows {
+    for window in list.windows(2) {
         resultv.push(format!(
             "For want of a {} the {} was lost.\n",
             window[0], window[1]
