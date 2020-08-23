6bba7e5e3b233805547d3ef45e17397c7aac514f
orhanbalci@gmail.com <orhanbalci@gmail.com>
2020-08-23 23:00:56 +0300

    anagram rust solution update

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,22 +2,20 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let mut word_slice = word.to_lowercase().chars().collect::<Vec<_>>();
+    let lowercase_word = word.to_lowercase();
     word_slice.sort();
     let length = word.chars().count();
     possible_anagrams
         .iter()
         .filter(|&possible| {
-            let mut pos = possible.to_lowercase().chars().collect::<Vec<_>>();
+            let lowercase_pos = possible.to_lowercase();
+            let mut pos = lowercase_pos.chars().collect::<Vec<_>>();
             pos.sort();
-            pos.iter()
-                .zip(word_slice.iter())
-                .filter(|(&a, &b)| a == b)
-                .count()
-                == length
+            pos == word_slice
                 && length > 0
-                && *possible.to_lowercase() != word.to_lowercase()
+                && lowercase_pos != lowercase_word 
                 && length == possible.chars().count()
         })
-        .map(|&t| t)
+        .copied()
         .collect::<HashSet<&str>>()
 }
